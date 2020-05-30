240eaa034692f42c17fbce902fc2f6aaff715c27
Damita Salmon <damita.salmon@gmail.com>
2020-05-30 14:25:17 -0400

    add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000
+    },
     module: {
         rules: [
             {
@@ -24,6 +28,12 @@ module.exports = {
                   { loader: 'style-loader' }, 
                   { loader: 'css-loader' }
               ]  
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    {loader: 'url-loader'}
+                ]
             }
         ]
     }
